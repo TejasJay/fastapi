0c08591f3371f95877d4f7306b7ffa9039c3f2f7
TejasJay <tejasjayprakash@gmail.com>
2025-12-05 19:01:42 -0500

    added security feature

--- Desktop/fastapi/schema.py
+++ Desktop/fastapi/schema.py
@@ -1,8 +1,68 @@
 from sqlmodel import SQLModel
 from typing import List, Optional
 
-class ProductBase(SQLModel):
+class UserBase(SQLModel):
+    username: str
+    role: str = "customer"
+
+class UserCreate(UserBase):
+    password: str
+
+class UserPublic(UserBase):
+    id: int
+
+class CategoryBase(SQLModel):
+    name: str
+
+class CategoryCreate(CategoryBase):
     pass
 
-class ProductCreate():
-    pass
+class CategoryPublic(CategoryBase):
+    id: int
+
+class ReviewBase(SQLModel):
+    text: str
+    rating: int
+
+class ReviewCreate(ReviewBase):
+    product_id: int
+    # user_id is thken from the authenticated session of the user
+
+class ReviewPublic(ReviewBase):
+    id: int
+    user: UserPublic
+
+class ProductBase(SQLModel):
+    name: str
+    description: str 
+    price: float
+
+class ProductCreate(ProductBase):
+    category_id: int
+
+class ProductPublic(ProductBase):
+    id: int
+    category: CategoryPublic
+    review: List[ReviewPublic]
+
+# To avoid circular imports, we can create specific models for nested data
+# that don't have their own nested relationships.
+
+class CategoryPublicWithProducts(CategoryPublic):
+    products: List[ProductPublic] = []
+
+class UserPublicWithReviews(UserPublic):
+    reviews: List[ReviewPublic] = []
+
+class OrderCreate(SQLModel):
+    customer_name: str
+    item: str
+
+class OrderResponse(SQLModel):
+    id: int
+    customer_name: str
+    item: str
+    status: str
+
+    class Config:
+        orm_mode = True
