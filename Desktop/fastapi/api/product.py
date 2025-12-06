de22c52c068e576b3b31e8abb7c7eb24f909b367
TejasJay <tejasjayprakash@gmail.com>
2025-12-05 23:47:52 -0500

    Created product api

---
+++ Desktop/fastapi/api/product.py
@@ -0,0 +1,31 @@
+from typing import List
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlmodel.ext.asyncio.session import AsyncSession
+from core.db import get_session
+from crud import crud_product
+from schema import ProductCreate, ProductPublic
+
+router = APIRouter()
+
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=ProductPublic)
+async def create_new_product(
+    product_data: ProductCreate,
+    session: AsyncSession = Depends(get_session)
+):
+    return await crud_product.create_product(product_data=product_data, session=session)
+
+@router.get("/", response_model=List[ProductPublic])
+async def get_all_products_list(
+    session: AsyncSession = Depends(get_session)
+):
+    return await crud_product.get_all_products(session=session)
+
+@router.get("/{product_id}", response_model=ProductPublic)
+async def get_product_details(
+    product_id: int,
+    session: AsyncSession = Depends(get_session)
+):
+    product = await crud_product.get_product_by_id(product_id=product_id, session=session)
+    if not product:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Product with ID {product_id} is not available")
+    return product
--- Desktop/fastapi/crud/crud_product.py
+++ Desktop/fastapi/crud/crud_product.py
@@ -2,4 +2,23 @@ from typing import List
 from sqlmodel import select
 from sqlmodel.ext.asyncio.session import AsyncSession
 from model.models import Product
+from schema import ProductCreate
+
+
+async def create_product(product_data: ProductCreate, session: AsyncSession) -> Product:
+    db_product = Product.model_validate(product_data)
+    session.add(db_product)
+    await session.commit()
+    await session.refresh(db_product)
+    return db_product
+
+async def get_all_products(session: AsyncSession) -> List[Product]:
+    statement = select(Product)
+    result = await session.exec(statement)
+    return result.all()
+
+async def get_product_by_id(product_id: int, session: AsyncSession) -> Product:
+    statement = select(Product).where(Product.id == product_id)
+    result = await session.exec(statement)
+    return result.one_or_none()    
 
